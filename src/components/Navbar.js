3e33e40145600d6ee5ed0f261a85060833ca794d
naveengitboi <naveenjangiti9@gmail.com>
2024-01-27 11:46:11 +0530

    added data

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { MdArrowOutward } from "react-icons/md";
+import '../css/Navbar.css'
+function Navbar() {
+    return (
+        <nav className='navbar'>
+            <div className="logo">
+                JN
+            </div>
+            <ul className='navlinks'>
+                <li><NavLink to='/' className='linkEle '>Work</NavLink></li>
+                <li><NavLink to='/info' className='linkEle'>info</NavLink></li>
+            </ul>
+            <div className="socialLinks">
+                <a href="" className='linkEle'>Insta<MdArrowOutward/> </a>
+                <a href="" className='linkEle'>Resume <MdArrowOutward/></a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/data.js
@@ -0,0 +1,111 @@
+export const profileInfo = {
+    name:"Jangiti Naveen",
+    
+}
+
+export const skillInfo = [
+    {
+        skill: "react js",
+        category: "web dev"
+    },
+    {
+        skill: "node js",
+        category: "web dev"
+    },
+    {
+        skill: "express js",
+        category: "web dev"
+    }, {
+        skill: "mongo db",
+        category: "web dev"
+    },
+
+    {
+        skill: "mongo db",
+        category: "Database"
+    },
+    {
+        skill: "Sqlite3",
+        category: "Database"
+    },
+
+    {
+        skill: "js",
+        category: "programming"
+    },
+    {
+        skill: "python (DSA)",
+        category: "programming"
+    },
+    {
+        skill: "c++(intermediate)",
+        category: "programming"
+    },
+    {
+        skill: "OOPs",
+        category: "programming"
+    },
+
+    {
+        skill: "Numpy",
+        category: "ai ml"
+    },
+
+    {
+        skill: "pandas",
+        category: "ai ml"
+    }, {
+        skill: "Matplotlib",
+        category: "ai ml"
+    },
+    {
+        skill: "scikit learn",
+        category: "ai ml"
+    },
+
+]
+
+export const experienceInfo = [
+    {
+        company: "CCD NITC",
+        position:"Web Developer",
+        interval: "06/06/23 - 30/07/23",
+        description: "Crafted website content highlighting campus remarkable placement statistics to attract and engage discerning recruiting firms."
+    },
+    {
+        company: "Freelancing",
+        position: "Graphic Designer",
+        interval: "Present",
+        description: "Engaged as a freelance designer, I participated in a collaborative venture focused on an intriguing assignment for a YouTube content producer."
+    },
+
+]
+
+
+export const projectsInfo = [
+    {
+        proj: "Youtube Clone",
+        projDescription: "Throughout my journey of replicating YouTubes exceptional UI design, I delved into React and backend tools, gaining valuable expertise in creating engaging user interfaces and powerful web applications.",
+        skillsUsed:["React JS", "Mongo DB", "Authentication", "REST API", "Routers"],
+        projLink:"https://github.com/naveengitboi/youtubeclone"
+    },
+    {
+        proj: "Student Learning Tracker",
+        projDescription: "Working on a website which track and keeps on eye on student learnings. Had a thought to integrate materials and more useful tools for any student who aims to keep daily streak in studies.This idea came when I realized I dont have a tool to keep track of my learning journey",
+        skillsUsed: ["React JS","Express", "Mongo DB", "Authentication", "REST API", "Routers"],
+        projLink: "https://github.com/naveengitboi/student_tracker"
+    },
+    {
+        proj: "Basic Dept clone",
+        projDescription: "I remaked a website characterized by exceptional elegance and impressive functionality, resulting in a seamless user experience using React JS, which helped me in knowing more usage of React JS",
+        skillsUsed: ["React JS", "Routers"],
+        projLink: "https://github.com/naveengitboi/basic_dept_clone"
+    },
+    {
+        proj: "Graphic Designfolio",
+        projDescription: "Executed a comprehensive project showcasing adept design skills, coupled with proficient utilization of Photoshop and various other design tools, underscoring my extensive experience",
+        skillsUsed: ["React JS", "Routers"],
+        projLink: "https://graphicdesignerau.netlify.app/",
+
+    },
+]
