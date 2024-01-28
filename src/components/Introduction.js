7c07c98a626827b2fa347a13bffc84e4d6dbb371
naveengitboi <naveenjangiti9@gmail.com>
2024-01-28 13:19:19 +0530

    done info section

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -2,9 +2,20 @@ import React from 'react'
 import { MdOutlineArrowDownward } from "react-icons/md";
 import '../css/CardItem.css'
 import '../css/Introduction.css'
+import { IoIosGitCompare } from "react-icons/io";
 function Introduction(props) {
     return (
         <div className="introcontainer">
+            <div className="introHeader">
+                <div className="dots">
+                    <span className='red'></span>
+                    <span className='orange'></span>
+                    <span className='green'></span>
+                </div>
+                <div className='leftIconHeader'>
+                    <IoIosGitCompare/>
+                </div>
+            </div>
             <div className="introInnerContainer">
                 <div className='largefont intro'>
                     <p>
@@ -12,8 +23,11 @@ function Introduction(props) {
                         wizard, <span onMouseOver={(e) => props.cursorsizeFunc(e)} onMouseLeave={(e) => props.cursorsizeLeaveFunc(e)} >graphic </span> virtuoso, and  <span onMouseOver={(e) => props.cursorsizeFunc(e)} onMouseLeave={(e) => props.cursorsizeLeaveFunc(e)} >artistic</span> soul
                     </p>
                 </div>
+
+                <div className="arrowDown"><MdOutlineArrowDownward /></div>
+            </div>
+
         </div>
-       </div>
     )
 }
 
