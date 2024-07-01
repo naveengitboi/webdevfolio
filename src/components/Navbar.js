f8e102383e636e31bccd34b4b3bbe0d5f8511d66
naveengitboi <naveenjangiti9@gmail.com>
2024-07-01 23:08:22 +0530

    added navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,116 @@
-import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
-import { MdArrowOutward } from "react-icons/md";
+import React, { useState , useEffect, useRef} from "react";
+import { NavLink } from "react-router-dom";
+import {MdArrowCross, MdArrowOutward } from "react-icons/md";
 import { RxHamburgerMenu } from "react-icons/rx";
-import '../css/Navbar.css'
+import { TiPointOfInterest } from "react-icons/ti";
+import "../css/Navbar.css";
 function Navbar(props) {
-    const { textEff } = props;
-    const [ham, setHam] = useState(false)
-    const handleHamBurger = () => {
-        setHam((prev) => !prev)
-
+  const { textEff } = props;
+  const [ham, setHam] = useState(false);
+  const handleHamBurger = () => {
+    setHam((prev) => !prev);
+  };  
+  const navref = useRef()
+  useEffect(() => {
+   const handler = (e) => {
+    if(!navref.current.contains(e.target)){
+      console.log(e.target)
+      setHam((prev) => false)
+    }false
     }
 
-    return (
-        <nav className='navbar' >
-            <div className="logo" data-initial="JNAVEEN" onMouseOver={(e) => textEff(e)}>
-                JNAVEEN
-            </div>
-            <ul className='navlinks'>
-                <li><NavLink to='/' className='linkEle '>Work</NavLink></li>
-                <li><NavLink to='/info' className='linkEle'>info</NavLink></li>
-            </ul>
-            <div className="socialLinks">
-                <a href="https://www.instagram.com/_atrociuos_arts/" rel="noreferrer" target='_blank' className='linkEle'  >Insta<MdArrowOutward /> </a>
-                <a href="/assets/resume.pdf" rel="noreferrer" target='_blank' className='linkEle'>Resume <MdArrowOutward /></a>
-            </div>
-            <div className="hamburgerMenu">
-                <RxHamburgerMenu onClick={handleHamBurger} />
-            </div>
+    document.addEventListener('click', handler)
 
-            <ul className={ham ? "mobileNavlinks mobileLinksActive" : "mobileNavlinks"}>
-                <li><NavLink to='/' className='linkEle '>Work</NavLink></li>
-                <li><NavLink to='/info' className='linkEle'>info</NavLink></li>
-                <li><a href="https://www.instagram.com/_atrociuos_arts/" rel="noreferrer" target='_blank' className='linkEle'  >Insta<MdArrowOutward /> </a></li>
-                <li><a href="/assets/resume.pdf" rel="noreferrer" target='_blank' className='linkEle'>Resume <MdArrowOutward /></a></li>
+    return document.removeEventListener('click', handler, )
+  }, [ham])
+  
+  return (
+    <nav className="navbar">
+      <div
+        className="logo"
+        data-initial="JNAVEEN"
+        onMouseOver={(e) => textEff(e)}
+      >
+        JNAVEEN
+      </div>
+      <ul className="navlinks">
+        <li>
+          <NavLink to="/" className="linkEle ">
+            Work
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/info" className="linkEle">
+            info
+          </NavLink>
+        </li>
+      </ul>
+      <div className="socialLinks">
+        <a
+          href="https://www.instagram.com/_atrociuos_arts/"
+          rel="noreferrer"
+          target="_blank"
+          className="linkEle"
+        >
+          Insta
+          <MdArrowOutward />{" "}
+        </a>
+        <a
+          href="/assets/resume.pdf"
+          rel="noreferrer"
+          target="_blank"
+          className="linkEle"
+        >
+          Resume <MdArrowOutward />
+        </a>
+      </div>
+      <div className="hamburgerMenu">
+        {
+          !ham? (<RxHamburgerMenu onClick={handleHamBurger} />):
+            (<TiPointOfInterest onClick={handleHamBurger} />)
+        }
 
-            </ul>
-            {/* <div className="glossyEffect"></div> */}
+      </div>
 
-        </nav>
-    )
+      <ul ref={navref}
+        className={ham ? "mobileNavlinks mobileLinksActive" : "mobileNavlinks"}
+      >
+        <li>
+          <NavLink to="/" className="linkEle ">
+            Work
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/info" className="linkEle">
+            info
+          </NavLink>
+        </li>
+        <li>
+          <a
+          href="https://www.instagram.com/_atrociuos_arts/"
+          rel="noreferrer"
+          target="_blank"
+          className="linkEle"
+        >
+          Insta
+          <MdArrowOutward />{" "}
+        </a>
+      </li>
+      <li>
+        <a
+          href="/assets/resume.pdf"
+          rel="noreferrer"
+          target="_blank"
+          className="linkEle"
+        >
+          Resume <MdArrowOutward />
+        </a>
+            </li>
+  </ul>
+{/* <div className="glossyEffect"></div> */}
+</nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
+
