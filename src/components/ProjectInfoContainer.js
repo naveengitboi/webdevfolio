440de8b38cbeb486fdf45cde2424260531acdb6c
naveengitboi <naveenjangiti9@gmail.com>
2024-07-03 20:48:37 +0530

    added css

---
+++ src/components/ProjectInfoContainer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ProjectInfoContainer = () => {
+  return <>
+      <div className="PICWrapper">
+        <div className="PICDetails">
+          <div className="">
+          <p className="PICProjectNamed>Test Hub</p>
+    
+        </div>
+          
+      </div>
+        <div className="PICOverview">
+              <p className="">Overview</p>
+              <p>Test hub waas companion web app originally build to be used to stadia game</p>
+        </div>
+      </div>
+  </>;
+};
+
+export default ProjectInfoContainer;
