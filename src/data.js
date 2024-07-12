c80e0580cd6d315648f11a0ed30b53091c16c5af
naveengitboi <naveenjangiti9@gmail.com>
2024-07-12 10:28:00 +0530

    add ps basics

--- src/data.js
+++ src/data.js
@@ -397,6 +397,264 @@ export const moreAboutProject = [
         ],
       }
     ]
+  },
+  {
+    projectName: 'Graphic Designfolio',
+    introduction: {
+      name: 'GDFolio',
+      description: 'Graphic Design Portfolio - 2021',
+      img: '/assets/works/ps/0.png',
+      bgColor: '#073F6E'
+    },
+    summary: {
+
+      role: {
+        type: 'Developer',
+
+        context: 'Designing and Developing'
+
+      },
+      isTeam: false,
+      own: 'Crafted from basics to showcase my design skills',
+      timeline: '7 Days',
+      completed: true,
+      progress: 'Deployed in Netlify',
+      overview: ['Designed this Portfolio to showcase my photoshop skills, that I developed while I was working with development on the other side.', 'I have good touch with drawings as well apart from designing and developing.', 'This was the 3rd project when I started learning React from youtube. Despite its basic in UI, I learned a lot while working on this project.']
+    },
+
+
+
+
+
+    projectDetails: [
+      {
+        isHeaderExist: true,
+        headerContent: {
+          isHeader: true,
+          type: 'Problem',
+          typeHeader: 'How to share my PS files? ',
+          typeSubHeader: 'One place for everyone to reach out for!',
+          typeContent: 'It became obvious that I cannot send my drive files to someone who wants to take a look on Photoshop Skills. So I wanted to develop simple ui site that showcases my PS skills.'
+        },
+        isSupportContentExist: false,
+        isBlocksExist: true,
+        blocksColor: RED_COLOR,
+        blocks: [
+          {
+            isFinal: false,
+            icon: <PiFigmaLogoDuotone />,
+
+            header: 'Inspiration',
+            content: 'Going through multiple websites to get idea what Im gonna build',
+            useNums: true,
+          },
+          {
+            isFinal: false,
+            icon: <PiGameControllerDuotone />,
+            header: 'Initiation',
+            content: 'Setting things ready to work with  website',
+            useNums: true,
+          },
+          {
+            icon: <GiClubs />,
+            isFinal: false,
+            header: 'Skill',
+            content: 'Yet this was my third project in react, Im confident enough to take up on.',
+            useNums: true,
+          }
+        ],
+      },
+      {
+        isHeaderExist: true,
+        headerContent: {
+          isHeader: true,
+          type: 'Idea',
+          typeHeader: 'Basic Design flow',
+          typeSubHeader: 'I have lame and basic design layout for the website in mind!',
+          typeContent: "I kept thinking of what could be the layout of website! and I came up with a basic layout design that can effectivly work."
+        },
+        isSupportContentExist: true,
+        isBlocksExist: false,
+        supportContent: [
+          {
+            isImage: true,
+            data: {
+              text: 'One.O  Layout of the website',
+              icon: <GiClubs />,
+              img: '/assets/works/ps/2.png'
+            }
+          },
+        ]
+
+      },
+      {
+        isHeaderExist: true,
+        headerContent: {
+          isHeader: true,
+          type: 'Glimpse',
+          typeHeader: 'Understanding Core',
+          typeSubHeader: 'Working of Reusable Components',
+          typeContent: "Built Reusable componenents like forms, transation table, history table, Graphs/ Analytics, Register, Login forms, User profile, so on. These sort of reusability enhanced the entire work flow of the website."
+        },
+        isSupportContentExist: true,
+        isBlocksExist: false,
+        supportContent: [
+          {
+            isImage: true,
+            data: {
+              text: 'Two.O Glimpse of Small Components',
+              icon: <GiClubs />,
+              img: '/assets/works/wm/1.png'
+            }
+          },
+          {
+            isImage: false,
+            data: {
+              isHeader: false,
+              typeSubHeader: 'Its interesting and challanging.',
+              typeContent: 'Crafted reusable components that streamlined my workflow, making tasks more efficient and streamlined. These components now enable me to tackle projects with ease and consistency.'
+            }
+          },
+          {
+            isImage: true,
+            data: {
+              text: 'Three.O Forgot password component',
+              icon: <GiClubs />,
+              img: '/assets/works/wm/2.png'
+            }
+          },
+          {
+            isImage: false,
+            data: {
+              isHeader: false,
+              typeSubHeader: 'History/ Heart of website',
+              typeContent: 'Created all sort of functionality a actual or a real website has. More functionality and more user friendly, Downloading history is what everyone wanted.'
+            }
+          },
+          {
+            isImage: true,
+            data: {
+              text: 'Four.O Very Recent history',
+              icon: <GiClubs />,
+              img: '/assets/works/wm/4.png'
+            }
+          },
+          {
+            isImage: true,
+            data: {
+              text: 'Five.O All Transaction history',
+              icon: <GiClubs />,
+              img: '/assets/works/wm/5.png'
+            }
+          }
+
+        ]
+      },
+      {
+        isHeaderExist: false,
+        isSupportContentExist: false,
+        isBlocksExist: true,
+        blocksColor: BLUE_COLOR,
+        blocks: [
+          {
+            isFinal: true,
+            icon: <PiFigmaLogoDuotone />,
+            note: 'Findings',
+
+            header: 'Things became more tricky to understand when lines are increasing, in high frequency! But No stops!',
+            useNums: false
+
+          }
+        ]
+      },
+      {
+        isHeaderExist: false,
+        isSupportContentExist: true,
+        isBlocksExist: false,
+        supportContent: [
+          {
+            isImage: false,
+            data: {
+              isHeader: false,
+              typeSubHeader: 'Entry Point?',
+              typeContent: 'Integrated Firebase Authentication for google login. Apart from that, actual Registration and login procedure is also followed. This helps the user to not focus on form filling, and hence ease in entering application.'
+            }
+          },
+
+          {
+            isImage: true,
+            data: {
+              text: 'Six.0 Entry to site',
+              icon: <GiClubs />,
+              img: '/assets/works/wm/7.png'
+            }
+          },
+          {
+            isImage: false,
+            data: {
+              isHeader: false,
+              typeSubHeader: 'One page for all options!',
+              typeContent: 'You have everything on table. Just know what to do. You have option to change password, 2factor authentication, deleting account, so on...'
+            }
+          },
+          {
+            isImage: true,
+            data: {
+              text: 'Seven.O All settings',
+              icon: <GiClubs />,
+              img: '/assets/works/wm/9.png'
+            }
+          },
+          {
+            isImage: true,
+            data: {
+              text: 'Eight.O Deleting Account',
+              icon: <GiClubs />,
+              img: '/assets/works/wm/3.png'
+            }
+          },
+
+
+        ]
+      },
+      {
+        isHeaderExist: true,
+        headerContent: {
+          isHeader: true,
+          type: 'Output',
+          typeHeader: 'Finally we reach end of sea.',
+          typeSubHeader: 'This project enriched my design skills as well as coding skills. ',
+          typeContent: "It exposed me to different tools and errors. Like Apex charts, React Charts, Data integration with charts, Axios Cors Errors, Deploying errors, Protected Routes, Authentications with firebase, React and express routers, MongodB  so on...."
+        },
+        isSupportContentExist: false,
+        isBlocksExist: true,
+        blocksColor: GREEN_COLOR,
+        blocks: [
+          {
+            isFinal: false,
+            icon: <PiFigmaLogoDuotone />,
+
+            header: 'Completed Figma',
+            content: 'Learned creating components, maskings, design principles so on.',
+            useNums: true,
+          },
+          {
+            isFinal: false,
+            icon: <PiGameControllerDuotone />,
+            header: 'Decoded Figma',
+            content: 'Decoded various task by breaking them into chunks and components to build website.',
+            useNums: true,
+          },
+          {
+            icon: <GiClubs />,
+            isFinal: false,
+            header: 'Seekings',
+            content: 'Learned a lot of things in building website, as I explained above',
+            useNums: true,
+          }
+        ],
+      }
+    ]
   }
 
 ]
